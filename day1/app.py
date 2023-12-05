90936b84ae6ae7ec0b186f28f123616fdb61390c
Manigandan Elumalai <manielumalai@hipagesgroup.com.au>
2023-12-05 11:54:38 +1100

    day 1 part 2

--- day1/app.py
+++ day1/app.py
@@ -1,11 +1,30 @@
 import re
-import inputs
-#inputs.inputs = ["1abc2", "398878", "a1b2c3d4e5f", "treb7uchet"]
+import inputdata
+#inputs = ["two1nine", "eightwothree", "abcone2threexyz", "xtwone3four", "4nineeightseven2", "zoneight234", "7pqrstsixteen"]
+inputs = inputdata.inputs
+
+def convert_to_numbers(s):
+    words_to_numbers = {
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+        'zero': '0'
+    }
+
+    pattern = re.compile(r'(' + '|'.join(words_to_numbers.keys()) + r')')
+    
+    return re.sub(pattern, lambda x: words_to_numbers[x.group()], s)
 
 sum = 0
-for input in inputs.inputs:
-    input_nums = re.findall(r'\d', input)
+for input in inputs:
+    input_nums = re.findall(r'\d', convert_to_numbers(input))
     num = input_nums[0]+input_nums[-1]
-    #print(num)
+    print(input + " : " + convert_to_numbers(input) + " : " + num)
     sum += int(num)
 print(sum)
